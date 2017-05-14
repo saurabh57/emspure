0ae4108f0d01aa02ce21a2ad3094e94b4d640528
System Administrator <saurabh.panday57@gmail.com>
2017-05-14 17:58:21 +0530

    scroll SB

--- frontend/app/components/ProfileSummaryCard.js
+++ frontend/app/components/ProfileSummaryCard.js
@@ -4,14 +4,35 @@ import styles from 'styles/components/profileSummaryCard'
 class ProfileSummaryCard extends Component{
 	constructor(props){
 		super(props);
+		this.state = {
+			styleScroll: { transform: 'translateY(0px)' },
+		}
+		this.handleScroll = this.handleScroll.bind(this);
 	}
+
+	componentDidMount() {
+	    window.addEventListener('scroll', this.handleScroll);
+	}
+
+	componentWillUnmount() {
+	    window.removeEventListener('scroll', this.handleScroll);
+	}
+
+	handleScroll(event){
+	    let scrollTop = event.srcElement.body.scrollTop,
+	        itemTranslate = scrollTop > 60 ? scrollTop - 60 : 60 - scrollTop;
+	    let styleScroll = {};
+	    styleScroll.transform = `translateY(${itemTranslate}px)`;
+	    this.setState({ styleScroll });
+	};
+
 	render(){
 		return(
-			<div className={styles.profileSummaryCard}>
+			<div className={styles.profileSummaryCard} style={this.state.styleScroll}>
 				<ProfilePic />
 				<BasicInfo />
 			</div>
-			)
+		)
 	}
 }
 
