7d5548f72875755714aa1d6cfd8907c492e37f93
Saurabh Panday <saurabh.panday@90-v1rtg8wp-32h.dlink>
2017-05-21 14:24:13 +0530

    Tab css change

--- frontend/app/sources/OrganisationDefault.js
+++ frontend/app/sources/OrganisationDefault.js
@@ -50,6 +50,214 @@ const OrganisationDefault = [
     		updateCount: 2,
     	}
     ]
+  },
+  {
+    orgName: 'DEF Coaching',
+    location: 'Delhi',
+    orgNotification: [
+      {
+    		displayText: 'Notification',
+        iconClass: 'fa-bell-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Messages',
+        iconClass: 'fa-envelope-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Assignments',
+        iconClass: 'fa-code',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Contents',
+        iconClass: 'fa-file-o',
+    		updateCount: 2,
+    	}
+    ]
+  },
+  {
+    orgName: 'DEF Coaching',
+    location: 'Delhi',
+    orgNotification: [
+      {
+    		displayText: 'Notification',
+        iconClass: 'fa-bell-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Messages',
+        iconClass: 'fa-envelope-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Assignments',
+        iconClass: 'fa-code',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Contents',
+        iconClass: 'fa-file-o',
+    		updateCount: 2,
+    	}
+    ]
+  },
+  {
+    orgName: 'DEF Coaching',
+    location: 'Delhi',
+    orgNotification: [
+      {
+    		displayText: 'Notification',
+        iconClass: 'fa-bell-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Messages',
+        iconClass: 'fa-envelope-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Assignments',
+        iconClass: 'fa-code',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Contents',
+        iconClass: 'fa-file-o',
+    		updateCount: 2,
+    	}
+    ]
+  },
+  {
+    orgName: 'DEF Coaching',
+    location: 'Delhi',
+    orgNotification: [
+      {
+    		displayText: 'Notification',
+        iconClass: 'fa-bell-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Messages',
+        iconClass: 'fa-envelope-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Assignments',
+        iconClass: 'fa-code',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Contents',
+        iconClass: 'fa-file-o',
+    		updateCount: 2,
+    	}
+    ]
+  },
+  {
+    orgName: 'DEF Coaching',
+    location: 'Delhi',
+    orgNotification: [
+      {
+    		displayText: 'Notification',
+        iconClass: 'fa-bell-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Messages',
+        iconClass: 'fa-envelope-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Assignments',
+        iconClass: 'fa-code',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Contents',
+        iconClass: 'fa-file-o',
+    		updateCount: 2,
+    	}
+    ]
+  },
+  {
+    orgName: 'DEF Coaching',
+    location: 'Delhi',
+    orgNotification: [
+      {
+    		displayText: 'Notification',
+        iconClass: 'fa-bell-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Messages',
+        iconClass: 'fa-envelope-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Assignments',
+        iconClass: 'fa-code',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Contents',
+        iconClass: 'fa-file-o',
+    		updateCount: 2,
+    	}
+    ]
+  },
+  {
+    orgName: 'DEF Coaching',
+    location: 'Delhi',
+    orgNotification: [
+      {
+    		displayText: 'Notification',
+        iconClass: 'fa-bell-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Messages',
+        iconClass: 'fa-envelope-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Assignments',
+        iconClass: 'fa-code',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Contents',
+        iconClass: 'fa-file-o',
+    		updateCount: 2,
+    	}
+    ]
+  },
+  {
+    orgName: 'DEF Coaching',
+    location: 'Delhi',
+    orgNotification: [
+      {
+    		displayText: 'Notification',
+        iconClass: 'fa-bell-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Messages',
+        iconClass: 'fa-envelope-o',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Assignments',
+        iconClass: 'fa-code',
+    		updateCount: 2,
+    	},
+      {
+    		displayText: 'Contents',
+        iconClass: 'fa-file-o',
+    		updateCount: 2,
+    	}
+    ]
   }
 ];
 
