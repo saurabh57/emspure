6b13400a13e6c1312d33bfb1d327980e64a6302c
Suryajeet <suryajeet@adminisatorsAir.Dlink>
2017-07-01 16:29:09 +0530

    Expander

--- frontend/app/components/common/Expander.js
+++ frontend/app/components/common/Expander.js
@@ -1,51 +1,38 @@
 import React , {Component} from 'react';
 import styles from 'styles/components/common/expander';
+
 class Expander extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
 			expanded: false
 		};
-		this.getInitialState = this.getInitialState.bind(this);
 		this.toggleText = this.toggleText.bind(this);
 		this.getExpanderText = this.getExpanderText.bind(this);
 	}
-	getInitialState() {
-        return {
-           expanded: false
-       };
-    }
 
-    toggleText() {
-    	if(this.state.expanded){
-    		this.setState({
-	            expanded: false
-	        });
-    	} else {
-    		this.setState({
-	            expanded: true
-	        });
-    	}
-    }
+  toggleText() {
+		this.setState({ expanded: !this.state.expanded });
+  }
 
-    getExpanderText(expText, thLength) {
-    	let divText = expText;
-        if (!this.state.expanded) {
-        	divText = expText.substring(0, thLength || 86);
-        }
-        return <div className = {styles.expanderContent}> {divText} </div>;
+  getExpanderText (expText, thLength) {
+  	let divText = expText;
+    if (!this.state.expanded) {
+    	divText = expText.substring(0, thLength || 86);
     }
+    return <div className = {styles.expanderContent}> {divText} </div>;
+  }
 
 	render(){
 		const expText = this.props.expText;
 		const thresholdLength = this.props.thresholdLength;
 		var expandedDiv = this.getExpanderText(expText, thresholdLength);
-	    return (
-	        <div>
-	            { expandedDiv }
-	            <a onClick={this.toggleText}>Read more</a>
-	        </div>
-	    );
+    return (
+      <div>
+        { expandedDiv }
+        <a onClick={this.toggleText}>Read more</a>
+      </div>
+    );
 	}
 }
 
