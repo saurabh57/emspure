08065e447446dcf542eba8a29cb8db05c7d535d6
arvind <arvindsinghnitp@gmail.com>
2017-07-01 12:29:58 +0530

    user detail routes added

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -46,6 +46,8 @@ module.exports.routes = {
 
   'post /signin': 'UserController.signin',
   'post /signup': 'UserController.signup',
+  'put /userDetails': 'UserController.updateUser',
+  'delete /userDetails': 'UserController.deleteUser',
 
 
 
